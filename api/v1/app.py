8bf7042d16c740f30610141c7501efeeaf07a6d9
root <samuelserenaluv@gmail.com>
2024-04-29 07:18:56 -0700

    fixed commit issues

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,35 @@
-from os import getenv
+#!/usr/bin/python3
+"""Flask server (variable app)
+"""
+
 
 from flask import Flask, jsonify
 from models import storage
+from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({"error": "Not found"}), 404
-
-
-def teardown(error=None):
+@app.teardown_appcontext
+def downtear(self):
+    '''Status of your API'''
     storage.close()
 
 
-app.teardown_appcontext(teardown)
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify({'error': 'Not found'}), 404
 
-if __name__ == '__main__':
-    host = getenv("HBNB_API_HOST", '0.0.0.0')
-    port = int(getenv("HBNB_API_PORT", 5000))
 
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,13 @@
-from api.v1.views import app_views
+#!/usr/bin/python3xx
+'''api status'''
+import models
+from models import storage
+from models.base_model import BaseModel
 from flask import jsonify
+from api.v1.views import app_views
 
 
-@app_views.route('/status')
-def status():
-    response = {"status": "OK"}
-    return jsonify(response)
+@app_views.route('/status', strict_slashes=False)
+def returnstuff():
+    '''return stuff'''
+    return jsonify(status='OK')
